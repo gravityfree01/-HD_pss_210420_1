f025ed2b63cfbd3d65f7d09297f591b0b6872872
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-20 20:45:57 +0900

    함수 및 Main.h 과  Main.cpp 파일 준비

---
+++ 스터디 보강 함수 및 헤더파일 소스파일 만들기/Project1/Main.h
@@ -0,0 +1,38 @@
+#ifndef __HEADER_H_
+#define __HEADER_H_
+
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+/*
+
+*/
+// 파라미터에 따라 구구단 값을 출력해주빈다.
+void Print9(int number) {
+	for (int i = 1; i <= 9; i++) {
+		printf("\n %d*%d=%d", number, i, number * i);
+	}
+}
+
+
+bool IsErrorNumber(int number) {
+	if (number < 0 || number > 9) return true;
+
+	return false;
+}
+
+/*
+int Sum(int a, int b) {
+	return a + b;
+}
+
+int Sub(int a, int b) {
+	return a - b;
+}
+
+*/
+#endif
